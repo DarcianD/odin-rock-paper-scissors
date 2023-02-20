f8fdb50bb738309fb43f59500754f52bf791d62b
DarcianD <darciandinh@gmail.com>
2023-02-19 22:33:36 -0600

    added player choice func, console log results

--- script.js
+++ script.js
@@ -1,59 +1,46 @@
-function game(){
+const bear = document.getElementById("bear");
+const hunter = document.getElementById("hunter");
+const ninja = document.getElementById("ninja");
 
+    function getComputerChoice() {
+        const compChoices = ['BEAR', 'HUNTER', 'NINJA']; // array [0 1 2]
+        const randNum = Math.floor(Math.random()* 3);; // generate random number 0 ,1, 2
+        return compChoices[randNum]; // choice [either 0, 1, or 2]
+    }
 
-    for(let i =0; i < 5; i++){
+    function getPlayerChoice(){  // activate this function output user select
+        bear.addEventListener('click', function(){
+            game("BEAR") 
+        })
+        hunter.addEventListener('click', function(){
+            game("HUNTER") 
+        })
+        ninja.addEventListener('click', function(){
+            game("NINJA") 
+        })
+    }
 
-        let enterPromt = prompt("Bear, Ninja, or Hunter?"); // user input
-        let playerSelection = enterPromt.toUpperCase(); // turn input all into uppercase
+    function game(playerSelection){
         const computerSelection = getComputerChoice();
+        console.log("Player Selection: " + playerSelection);
+        console.log("Computer Selection: " + computerSelection);
 
-        function getComputerChoice() {
-            const choices = ['BEAR', 'HUNTER', 'NINJA']; // array [0 1 2]
-            const randNum = Math.floor(Math.random()* 3);; // generate random number 0 ,1, 2
-            return choices[randNum]; // choice [either 0, 1, or 2]
-        }
+        if( playerSelection === computerSelection ){
+            console.log("UHHH IT'S A TIE")
+            } else if( playerSelection === "BEAR" ){
+                if( computerSelection === "NINJA")
+                return console.log("BEAR EAT NINJA! YOU WIN")
+                else return console.log("HUNTER SHOOT BEAR! YOU LOST!")
+            } else if( playerSelection === "HUNTER" ){
+                if( computerSelection === "BEAR")
+                return console.log("HUNTER SHOOT BEAR! YOU WIN")
+                else return console.log("NINJA KILL HUNTER! YOU LOST!")
+            } else if( playerSelection === "NINJA" ){
+                if( computerSelection === "HUNTER")
+                return console.log("NINJA KILL HUNTER! YOU WON!")
+                else return console.log("BEAR EAT NINJA! YOU LOST!")
+            }
 
+    }
 
-                function playRound(playerSelection,computerSelection) {
-                        if( playerSelection === computerSelection ){
-                        return "IT'S A TIE!" 
-                        }
-
-                        if( playerSelection === "BEAR" ){
-                            if( computerSelection === "NINJA")
-                            return "YOU WIN! BEAR EAT NINJA"
-                            else  return "YOU LOSE! HUNTER SHOOT BEAR"
-                        }
-                        
-                        if( playerSelection === "HUNTER" ){
-                            if( computerSelection === "BEAR")
-                            return "YOU WIN! HUNTER SHOOT BEAR"
-                            else  return "YOU LOSE! NINJA KILL HUNTER"
-                        }
-
-                        if( playerSelection === "NINJA" ){
-                            if( computerSelection === "HUNTER")
-                            return "YOU WIN! NINJA KILL HUNTER"
-                            else  return "YOU LOSE! BEAR EAT NINJA"
-                        }
-                    };
-
-console.log(playerSelection);
-console.log(computerSelection);
-//console.log(getComputerChoice(computerSelection));
-console.log(playRound(playerSelection,computerSelection));        
-}
-}
-console.log(game());
-
-
-
-
-
-
-
-
-
-
-
-
+    getPlayerChoice();
